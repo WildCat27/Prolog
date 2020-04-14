a8f231820dac09044ad02280d626b9ac2383631d
WildCat27 <WildCat27@users.noreply.github.com>
2020-04-14 22:42:39 +0300

    отредактировала индивидуалку 1

--- Individual task 1.pl
+++ Individual task 1.pl
@@ -1,15 +1,23 @@
 
-is_pent(P):-pent(1,P).
-pent(N,P):-Pent is N*(3*N-1)/2,(Pent =< P),(Pent = P,!;N1 is N+1,pent(N1,P)).
+is_pent(P):-is_pent(1,P).
+is_pent(N,Pent):-pent(N,Pent),!.
+is_pent(N,P):-pent(N,Pent),Pent < P,N1 is N+1,is_pent(N1,P).
 
 task:-task(1,1).
-task(J,K):-pentagonal(J,Pj),pentagonal(K,Pk),SUM is Pj+Pk,is_pent(SUM),write(SUM).
-task(J,K):-(J < 500),J1 is J+1,task(J1,K).
-task(J,K):-(J = 500),(K < 500),K1 is K+1,task(K1,K1).
-pentagonal(N,P):-P is N*(3*N-1)/2.
+task(J,K):-pent(J,Pj),pent(K,Pk),Sum is Pj+Pk,is_pent(Sum),Dif is Pj-Pk,is_pent(Dif),write(J),nl,write(K),fail.
+task(J,K):-J < 900,J1 is J+1,task(J1,K).
+task(900,K):-K < 900,K1 is K+1,task(K1,K1).
 
-j:-j(1,1).
-j(J,K):-JK is J*K,JK = 900,!.
-j(J,K):-(J < 30),J1 is J+1,j(J1,K).
-j(J,K):-(J = 30),(K < 30),K1 is K+1,j(K1,K1).
-is_pent(DIF),,DIF is Pj-Pk
+sum:-task_s(1,1).
+task_s(J,K):-pent(J,Pj),pent(K,Pk),Sum is Pj+Pk,is_pent(Sum),write(J),nl,write(K),nl,fail.
+task_s(J,K):-J < 900,J1 is J+1,task_s(J1,K).
+task_s(900,K):-K < 900,K1 is K+1,task_s(K1,K1).
+
+dif:-task_d(1,1).
+task_d(J,K):-pent(J,Pj),pent(K,Pk),Dif is Pj-Pk,is_pent(Dif),write(J),nl,write(K),nl,fail.
+task_d(J,K):-J < 900,J1 is J+1,task_d(J1,K).
+task_d(900,K):-K < 900,K1 is K+1,task_d(K1,K1).
+
+pent(N,P):-P is N*(3*N-1)/2.
+
+
