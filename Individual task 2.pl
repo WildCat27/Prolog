72b1b4cf3775224aa5970197e5cf1bde9e8a565b
WildCat27 <WildCat27@users.noreply.github.com>
2020-04-15 00:09:54 +0300

    отредактировала индивидуалку 2

--- Individual task 2.pl
+++ Individual task 2.pl
@@ -2,13 +2,13 @@ read_list(A,N):-r_1([],A,0,N).
 r_1(A,A,N,N):-!.
 r_1(B,A,I,N):-I1 is I+1,read(X),append(B,[X],B1),r_1(B1,A,I1,N).
 
-task:-read(N),read_list(A,N),insertion_sort(A,B),write(B).
+insertion_sort:-read(N),read_list(A,N),insertion_sort(A,B),write(B).
 
-insertion_sort(A,B):-ins_sort(A,[],B).
-ins_sort([],B1,B):-append([],B1,B),!.
-ins_sort([Head|Tail],B1,B):-insert(Head,B1,B2),ins_sort(Tail,B2,B).
-insert(P,A1,A):-insert(P,[],A1,A).
+insertion_sort(List,SortList):-insertion_sort(List,[],SortList).
+insertion_sort([],SortList,SortList):-!.
+insertion_sort([Head|Tail],SortSublist,SortList):-insert(Head,SortSublist,S),insertion_sort(Tail,S,SortList).
+
+insert(P,List,Result):-insert(P,[],List,Result).
 insert(P,Start,[],Result):-append(Start,[P],Result),!.
-insert(P,Start,[Head|Tail],Result):-(P =< Head),append(Start,[P],NewStart),append(NewStart,[Head|Tail],Result),!.
-insert(P,Start,[Head|Tail],Result):-append(Start,[Head],NewStart),insert(P,NewStart,Tail,Result).
-
+insert(P,Start,[Head|Tail],Result):-P > Head,append(Start,[Head],NewStart),insert(P,NewStart,Tail,Result),!.
+insert(P,Start,Tail,Result):-append(Start,[P|Tail],Result).
